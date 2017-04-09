65366f59f86f0caaaae0772af7dc483566f14244
Taikos <MacBookAirArthur@macbook-air-de-arthur.home>
2017-04-09 19:56:52 +0200

    6ème MAJ
    Add Rush00 and MiniProject01

---
+++ Piscine C - MINIPROJECT_01 - 42/ex01/nmatch.c
@@ -0,0 +1,12 @@
+int		nmatch(char *s1, char *s2)
+{
+	if ((*s1 == '\0') && (*s2 == '\0'))
+		return (1);
+	else if ((*s1 == '\0') && (*s2 == '*'))
+		return (nmatch(s1, s2 + 1));
+	else if (*s2 == '*')
+		return (nmatch(s1 + 1, s2) + nmatch(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (nmatch(s1 + 1, s2 + 1));
+	return (0);
+}
---
+++ Piscine C - RUSH_00 - 42/ex00/rush01.c
@@ -0,0 +1,41 @@
+void	ft_putchar(char c);
+
+void	ft_display(int i, int j, int a, int b)
+{
+	if (i == 0 && j == 0)
+		ft_putchar('/');
+	else if (i == 0 && j == a - 1)
+		ft_putchar('\\');
+	else if (i == b - 1 && j == 0)
+		ft_putchar('\\');
+	else if (i == b - 1 && j == a - 1)
+		ft_putchar('/');
+	else if ((i == 0 || i == b - 1) && (j > 0 && j < a - 1))
+		ft_putchar('*');
+	else if ((i > 0 && i < b - 1) && (j == 0 || j == a - 1))
+		ft_putchar('*');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (x > 0 && y > 0)
+	{
+		while (i < y)
+		{
+			j = 0;
+			while (j < x)
+			{
+				ft_display(i, j, x, y);
+				j++;
+			}
+			ft_putchar('\n');
+			i++;
+		}
+	}
+}
